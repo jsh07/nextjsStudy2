002c4e36fa5542690d2adb011f886911d057706a
lynx <coeducation97@gmail.com>
2024-01-30 20:36:30 +0900

    MySQL 연결 테스트

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
-export default function Home() {
+import { executeQuery } from "@/util/database";
+export default async function Home() {
+  let getdata = await executeQuery(
+    `SELECT * FROM example_table`
+  );
+  console.log(getdata[0]);
   return (
     <div>
-
+      { getdata[0].name }
     </div>
   );
 }
---
+++ util/database.js
@@ -0,0 +1,33 @@
+const { createPool } = require("mysql2");
+
+const pool = createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+pool.getConnection((err, conn) => {
+  if (err) console.log("Error connecting to db...");
+  else console.log("Connected to db...!");
+  conn.release();
+});
+
+const executeQuery = (query, arrParams) => {
+  return new Promise((resolve, reject) => {
+    try {
+      pool.query(query, arrParams, (err, data) => {
+        if (err) {
+          console.log("Error in executing the query");
+          reject(err);
+        }
+        resolve(data);
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+module.exports = { executeQuery };
